0071d150a4e6615c7e6904c92c4927143ec6a0f6
Efrén Manuel González Diéguez <efrenmanuelgonzalez@gmail.com>
2022-12-03 18:57:53 +0100

    pep 8

--- Day3/c1.py
+++ Day3/c1.py
@@ -1,32 +1,37 @@
 from functools import reduce
 
-def priority(inputSets):
-    
 
-    repeated=ord(reduce(lambda a,b: a & b, inputSets).pop())
+def priority(inputSets):
+
+    repeated = ord(reduce(lambda a, b: a & b, inputSets).pop())
     val = repeated-96 if repeated > 96 else repeated-38
     return val
+
 
 def makeSetArrayOfLine(line):
     return [set(line[:len(line)//2]), set(line[len(line)//2:])]
 
+
 def getLinePriority(line):
     return priority(makeSetArrayOfLine(line))
 
+
 def p1():
     with open("input1.txt", "r") as inputFile:
-        lines=inputFile.readlines()
+        lines = inputFile.readlines()
         return sum(map(getLinePriority, lines))
+
 
 def makeSetArrayFromArray(lines, linesPerSet):
     for i in range(0, len(lines), linesPerSet):
         yield([set(line) for line in lines[i:i+linesPerSet]])
 
+
 def p2():
     with open("input1.txt", "r") as inputFile:
-        lines=inputFile.read().split()
-        return sum(map(priority, makeSetArrayFromArray(lines,3)))  
-            
+        lines = inputFile.read().split()
+        return sum(map(priority, makeSetArrayFromArray(lines, 3)))
+
 
 print(p1())
 print(p2())
--- Day3/c1testing.py
+++ Day3/c1testing.py
@@ -1,31 +1,37 @@
 from functools import reduce
 
-def priority(inputSets):
-    
 
-    repeated=ord(reduce(lambda a, b: a & b, inputSets).pop())
+def priority(inputSets):
+
+    repeated = ord(reduce(lambda a, b: a & b, inputSets).pop())
     #val = repeated-96 if repeated > 96 else repeated-38
     val = (repeated-38) % 58
     return val
 
+
 def makeSetArrayOfLine(line):
     return [set(line[:len(line)//2]), set(line[len(line)//2:])]
+
 
 def getLinePriority(line):
     return priority(makeSetArrayOfLine(line))
 
+
 def p1(inputLines):
-    
+
     return sum(map(getLinePriority, inputLines))
+
 
 def makeSetArrayFromArray(lines, linesPerSet):
     for i in range(0, len(lines), linesPerSet):
-        yield([set(line) for line in lines[i:i +  linesPerSet]])
+        yield([set(line) for line in lines[i:i + linesPerSet]])
+
 
 def p2(inputLines):
-    return sum(map(priority, makeSetArrayFromArray(inputLines, 3)))  
-            
+    return sum(map(priority, makeSetArrayFromArray(inputLines, 3)))
+
+
 with open("input1.txt", "r") as inputFile:
-    lines=inputFile.read().split()
+    lines = inputFile.read().split()
     print(p1(lines))
     print(p2(lines))
